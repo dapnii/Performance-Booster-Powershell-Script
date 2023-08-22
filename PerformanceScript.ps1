c21cf5faf931f3175d270ef0367abbc455e63996
dapnii <116521500+dapnii@users.noreply.github.com>
2023-08-22 23:15:56 +0200

    updated messages

--- PerformanceScript.ps1
+++ PerformanceScript.ps1
@@ -15,40 +15,40 @@ $ChromeCache = "C:\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Def
 
 foreach ($Path in $TempPaths) {
     Get-ChildItem -Path $Path *.* -Recurse | Remove-Item -Force -Recurse
-    Write-Output "Files from $Path have been deleted"
+    Write-Output "Files from $Path have been deleted..."
 }
 
 if ($GetPowerPlan -eq "Power Scheme GUID: 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c  (High performance)") {
-    Write-Output "High performance mode was already enabled"
+    Write-Output "High performance mode was already enabled..."
 }
  else {
     powercfg /s SCHEME_MIN
-    Write-Output "High performance has been enabled"
+    Write-Output "High performance has been enabled..."
 }
 
 if ($GlobalUserDisabled -eq 0 -or $GlobalUserDisabled -eq $null) {
     $BackgroundApps | New-ItemProperty -Name 'GlobalUserDisabled' -Value 1
-    Write-Output "Background apps have been disabled" 
+    Write-Output "Background apps have been disabled..." 
 } else {
-    Write-Output "Background apps are already disabled"
+    Write-Output "Background apps are already disabled..."
 }
 
 foreach ($Version in $FirefoxVersions) {
     if (Test-Path C:\Users\$env:USERNAME\AppData\Local\Mozilla\Firefox\Profiles\$Version\cache2) {
         Remove-Item C:\Users\$env:USERNAME\AppData\Local\Mozilla\Firefox\Profiles\$Version\cache2 -Recurse
-        Write-Output "Firefox cache cleared"
+        Write-Output "Firefox cache cleared..."
         break
     } else {
-        Write-Output "Firefox cache folder could not be find"
+        Write-Output "Firefox cache folder could not be find..."
         break
     }
 }
 
 if (Test-Path $ChromeCache) {
     Remove-Item $ChromeCache -Recurse
-    Write-Output "Google Chrome cache cleared"
+    Write-Output "Google Chrome cache cleared..."
 } else {
-    Write-Output "Google Chrome cache folder could not be find"
+    Write-Output "Google Chrome cache folder could not be find..."
 }
 
-Read-Host -Prompt "PRESS ENTER TO EXIT"
+Read-Host -Prompt "PRESS ENTER TO EXIT..."
